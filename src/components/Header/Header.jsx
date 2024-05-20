548e908a10c61525a44b54a74b19bac1c16bf5bd
Manu <manillar12@gmail.com>
2024-05-20 16:20:00 +0200

    change stars rate input to SVG

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -26,7 +26,7 @@ const Header = () => {
 	const handleTheme = () => {
 		setDarkTheme(!darkTheme);
 		const elements = document.querySelectorAll(
-			"body, header, .request-info, h2, h3, h4, h5, p, i, a, label, input, form button, .PcCard, .info, footer, .slider "
+			"body, header, .request-info, h2, h3, h4, h5, p, i, a, label, input, form button, .PcCard, .info, footer, .slider, .rect "
 		);
 		elements.forEach((element) => {
 			if (darkTheme) {
