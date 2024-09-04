5105933da2840d3ffb6d615fe74018ae8df9ccc4
Manu <manillar12@gmail.com>
2024-09-04 16:49:35 +0200

    Refactor BrandFilter component to dynamically fetch and display unique computer brands

--- src/components/BrandFilter/BrandFilter.jsx
+++ src/components/BrandFilter/BrandFilter.jsx
@@ -1,10 +1,16 @@
-import React, { useContext } from "react";
+import React, { useContext, useEffect, useState } from "react";
 import "./BrandFilter.css";
 import { BrandContext } from "../../contexts/BrandContext";
+import computers from "../../utils/computers";
 
 const BrandFilter = () => {
 	const [checked, setChecked] = useContext(BrandContext);
-	const brands = ["Acer", "Alurin", "Apple", "Asus", "Lenovo", "LG"];
+	const [brands, setBrands] = useState([]);
+
+	useEffect(() => {
+		const uniqueBrands = [...new Set(computers.map((computer) => computer.brand))];
+		setBrands(uniqueBrands);
+	}, []);
 
 	const handleCheck = (brand) => {
 		setChecked((prevState) => ({ ...prevState, [brand]: !prevState[brand] }));
